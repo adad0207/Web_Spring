ae44ed96d5862dcb98ffc6bed6999728ae97f815
Gihak Yun <adad0207@naver.com>
2022-11-09 23:47:34 +0900

    mapper 생성

---
+++ Board/src/main/java/com/gery/mapper/BoardMapper.java
@@ -0,0 +1,22 @@
+package com.gery.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.gery.domain.BoardVO;
+
+public interface BoardMapper {
+
+	@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO vo);
+
+	public void insertSelectKey(BoardVO vo);
+
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+
+	public int update(BoardVO vo);
+}
