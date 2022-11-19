475deb96c443ffd331c131974735f337c832369d
adad0207 <adad0207@naver.com>
2022-11-19 20:22:48 +0900

    Controller 등록 처리

--- Board/src/main/java/com/gery/controller/BoardController.java
+++ Board/src/main/java/com/gery/controller/BoardController.java
@@ -9,9 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.gery.domain.BoardVO;
 import com.gery.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -35,5 +38,15 @@ public class BoardController {
 
 	}
 	
+	@PostMapping
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register : " + board);
+		
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
 	
 }
