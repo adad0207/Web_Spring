f9eaf32190d669b9adcd7d4354b7d55f4f278ecf
adad0207 <adad0207@naver.com>
2022-11-19 20:14:04 +0900

    BoardController를 통한 목록 처리

---
+++ Board/src/main/java/com/gery/controller/BoardController.java
@@ -0,0 +1,39 @@
+package com.gery.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gery.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		
+		model.addAttribute("list", service.getlist());
+
+	}
+	
+	
+}
